df168d4a777e25d04e2a66bee4234bd4bce0bbd8
Anchal Sarode <anchalsarode03@gmail.com>
2026-05-30 19:25:23 +0530

    fixed error in timepaas2

--- timepaas2.java
+++ timepaas2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class timepaas {
+public class timepaas2 {
     public static void main(String[] args) {
         Map<Integer,String> map = new HashMap<>();
         map.put(1,"Apple");
