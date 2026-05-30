95c6d39e1abd7c61896d52b3ffa6d04c2a3a1734
Anchal Sarode <anchalsarode03@gmail.com>
2026-05-30 17:27:54 +0530

    Initial commit

---
+++ timepaas.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class timepaas {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"Apple");
+        
+    }
+}
